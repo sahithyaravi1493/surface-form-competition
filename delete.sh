5eadb59f3f388fe72a432df24ff5d14b22c2a5ca
Sahithya Ravi <sahithyaravi1493@gmail.com>
2022-04-04 11:12:17 -0700

    added scripts

---
+++ delete.sh
@@ -0,0 +1,15 @@
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/arc-challenge/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/arc-easy/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/boolq/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/cb/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/copa/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/cqa/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/hellaswag/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/obqa/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/race-m/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/race-h/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/sst-2/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/sst-5/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/storycloze/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/trec/gpt2-dev.hist
+rm /ubc/cs/research/nlp/sahiravi/surface-form-competition/data/rte/gpt2-dev.hist 
